9d1e1dcb24aadc709e6dc7306f809681e7e518e2
Ana C <70434233+unicorncakebang@users.noreply.github.com>
2024-02-11 15:00:04 -0300

    Update Excluindo um aluno.py
    O código interligado ao arquivo 'Cadastrando aluno" mostra o algoritmo de exclusão de aluno do banco.

--- Excluindo um aluno.py
+++ Excluindo um aluno.py
@@ -1,1 +1,12 @@
+cursor.execute("DELETE FROM alunos WHERE cpf = 12455788)")
+banco.close() #fechamento do banco de dados
+print("Dado removido com sucesso do banco") #mensagem de remoção de dado
+#mostrar lista total  de alunos após a exclusão do dado /ou dados solicitados
+cursor.execute("SELECT * FROM alunos")
+
+#tratamento de erro , se caso houver algum problema durante a exclusão
+except sqlite3.Error as erro:
+ print("Houve um erro na remoção da informação solicitada , por favor repita a ação novamente")
+finally #mensagem para sinalizar o fim da execução do programa.
+ print(" Programa executado")
 
